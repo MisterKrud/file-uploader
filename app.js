f8569ac836c57a4f8a0d05849ef5c1b3717b13d5
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2026-01-14 07:08:32 +1100

    Change import of dotenv in hail Mary attempt at deployment

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv/config');
+require("dotenv").config();
 const express = require("express");
 const expressSession = require('express-session');
 const passport = require("passport");
