f43c599a7e5e1f96de35e8bc84d49ffdc014929b
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2026-01-13 22:54:54 +1100

    Change file name to pool

---
+++ db/pool.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const { Pool } = require('pg')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  connectionTimeoutMillis: 5000,
+  idleTimeoutMillis: 30000,
+})
+
+module.exports = pool
